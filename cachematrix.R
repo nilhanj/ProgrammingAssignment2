0599c4c500b890f6298c695be6088be48588e401
Nilhan Jayasinghe <nilhanj@gmail.com>
2014-11-23 21:32:10 +0000

    assignemtn done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,47 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These two functions allow you to create a special "matrix" object that caches its inverse.
+## Then aenable you to get the cached version of the inverse id it exists without having to 
+## compute the inverse again.
 
-## Write a short comment describing this function
+## makeCacheMatrix takes in a matrix and creates a cache object and stores the inverse 
+## of the matrix. 
 
-makeCacheMatrix <- function(x = matrix()) {
+makeCacheMatrix <- function(x = matrix()) { # takes a matrix object as argument  
 
+  inverse <- NULL  # create an object to store the matrix and initialise it to NULL
+
+  # Allows you to change the value of the matrix from outside and reset the inverse to NULL
+  set <- function(y) {
+    x <<- y
+    inverse <<- NULL
+  }
+
+  get <- function() x # returns the matrix object
+  setinverse <- function(solve) inverse <<- solve(x) # cache the inverse of the matrix 
+  getinverse <- function() inverse # return the inverse of the matrix
+ 
+  # creates the methods for manipulating the matrix object
+  list(set =set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## Computes, caches, and returns    matrix inverse
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        
+  inverse <- x$getinverse() # find inverse of matrix created by makeCacheMatrix
+  
+  # if inverse was not cahced i.e not NULL then return inverse
+  if(!is.null(inverse)) {
+    message("getting cached data")
+    return(inverse)
+  }
+  data <- x$get() # get the values of the matrix created by makeCacheMatrix 
+  inverse <- solve(data, ...) # use solve to calculate inverse of matrix
+  x$setinverse(inverse) # call the setmean function in makeCacheMatrix to set the new value
+  
+  inverse ## Return a matrix that is the inverse of 'x'
+  
+  
 }
